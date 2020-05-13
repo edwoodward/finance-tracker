07e3722355b4bfda1f6784c9da19ee020ed5f5ba
edwoodward <ecw1@rice.edu>
2020-05-12 20:52:49 -0500

    Added stock lookup,display and styling

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,18 @@
+class StocksController < ApplicationController
+
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render "users/portfolio"
+      else
+        flash[:alert] = "Please enter a valid stock ticker symbol"
+        redirect_to portfolio_path
+      end
+    else 
+      flash[:alert] = "A stock ticker symbol must be entered"
+      redirect_to portfolio_path
+    end
+    
+  end
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,10 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iexclient[:sandbox_api_key],
                                     secret_token: Rails.application.credentials.iexclient[:sandbox_api_secret],
                                     endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker)
+    begin
+      new(ticker: ticker, name: client.company(ticker).company_name, last_price: client.price(ticker))  
+    rescue => exception
+      return nil
+    end
   end
 end
